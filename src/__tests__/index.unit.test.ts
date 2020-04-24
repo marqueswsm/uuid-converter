85fc41dc78fd9cd727f6679ca6c402ae6667bba2
Wagner Marques <wagnermarques@Wagners-MacBook-Pro.local>
2020-04-24 01:04:04 -0300

    all tests created

--- src/__tests__/index.unit.test.ts
+++ src/__tests__/index.unit.test.ts
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import { Uuid } from '../index';
 
 describe('Index unit tests', () => {
@@ -5,6 +6,24 @@ describe('Index unit tests', () => {
     it('should return an Uuid', () => {
       const response = Uuid.generate();
       expect(response).toBeDefined();
+    });
+  });
+
+  describe('#stringToBinary', () => {
+    it('should receive an uuid and return a binary', () => {
+      const uuid = uuidv4();
+      const response = Uuid.stringToBinary(uuid);
+      expect(response).toBeDefined();
+      expect(response).toBeInstanceOf(Buffer);
+    });
+  });
+
+  describe('#binaryToString', () => {
+    it('should receive a binary and return an uuid', () => {
+      const binary = Uuid.stringToBinary(uuidv4());
+      const response = Uuid.binaryToString(binary);
+      expect(response).toBeDefined();
+      expect(typeof response).toBe('string');
     });
   });
 });
