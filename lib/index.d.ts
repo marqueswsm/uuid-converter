3432eadfff0bc6ac3d5e772d84acdbd08d544d69
Wagner Marques <wagnermarques@Wagners-MacBook-Pro.local>
2020-04-25 02:36:50 -0300

    remove main

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="node" />
+export declare function generate(): string;
+export declare function stringToBinary(uuidString: string): Buffer;
+export declare function binaryToString(uuidBuffer: Buffer): string;
