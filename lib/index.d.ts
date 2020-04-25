a72b2a66cedc45de15d4a2c56733848e8cf4feea
Wagner Marques <wagnermarques@Wagners-MacBook-Pro.local>
2020-04-25 02:51:59 -0300

    remove buid files

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-export declare function generate(): string;
-export declare function stringToBinary(uuidString: string): Buffer;
-export declare function binaryToString(uuidBuffer: Buffer): string;
