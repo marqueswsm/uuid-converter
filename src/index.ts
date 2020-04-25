a95f2af8f48f6866d18562a870261c9538f31612
Wagner Marques <wagnermarques@Wagners-MacBook-Pro.local>
2020-04-24 22:42:39 -0300

    improve the return functions

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 import { v4 as uuidv4 } from 'uuid';
 
-export const Uuid = {
-  generate: uuidv4,
+export function generate(): string {
+  return uuidv4();
+}
 
-  stringToBinary(uuidString: string): Buffer {
-    const hex = uuidString.replace(/-/g, '');
-    const buffer = Buffer.from(hex, 'hex');
-    if (buffer.length !== 16) throw new Error('Invalid UUID');
-    return buffer;
-  },
+export function stringToBinary(uuidString: string): Buffer {
+  const hex = uuidString.replace(/-/g, '');
+  const buffer = Buffer.from(hex, 'hex');
+  if (buffer.length !== 16) throw new Error('Invalid UUID');
+  return buffer;
+}
 
-  binaryToString(uuidBuffer: Buffer): string {
-    if (uuidBuffer.length !== 16) throw new Error('Invalid UUID');
-    const hex = uuidBuffer.toString('hex');
-    return [
-      hex.slice(0, 8),
-      hex.slice(8, 12),
-      hex.slice(12, 16),
-      hex.slice(16, 20),
-      hex.slice(20, 32),
-    ].join('-');
-  },
-};
+export function binaryToString(uuidBuffer: Buffer): string {
+  if (uuidBuffer.length !== 16) throw new Error('Invalid UUID');
+  const hex = uuidBuffer.toString('hex');
+  return [
+    hex.slice(0, 8),
+    hex.slice(8, 12),
+    hex.slice(12, 16),
+    hex.slice(16, 20),
+    hex.slice(20, 32),
+  ].join('-');
+}
